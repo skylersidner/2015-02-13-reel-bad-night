64753b4fa8d093f435e99feba30fa37facc25095
Skyler Sidner <malevolentdragon@gmail.com>
2015-03-17 14:09:48 -0500

    homepage now working;

--- controllers/index_controller.rb
+++ controllers/index_controller.rb
@@ -1,12 +1,15 @@
 get "/" do
-  # .search returns an array; this adjusts to the object, specifically
-  array = Event.search("current_event", 2)
-  @current_event = array[0]
-  array = Film.search("id", "#{@current_event.film_id}")
-  @current_film = array[0]
-  array = Event.search("current_event", 1)
-  @previous_event = array[0]
-  array = Film.search("id", "#{@previous_event.film_id}")
-  @previous_film = array[0]
+  @current_event = Event.find_by current_event: 2
+  # array = Event.search("current_event", 2)
+  # @current_event = array[0]
+  @current_film = Film.find_by id: @current_event.film_id
+  # array = Film.search("id", "#{@current_event.film_id}")
+  # @current_film = array[0]
+  @previous_event = Event.find_by current_event: 1
+  # array = Event.search("current_event", 1)
+  # @previous_event = array[0]
+  @previous_film = Film.find_by id: @previous_event.film_id
+  # array = Film.search("id", "#{@previous_event.film_id}")
+  # @previous_film = array[0]
   erb :"index/homepage"
 end
