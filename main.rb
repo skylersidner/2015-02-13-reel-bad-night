6192c0797f5d7bb034521e1f9ea6e9d540872a15
Skyler Sidner <malevolentdragon@gmail.com>
2015-03-20 10:43:04 -0500

    added DB definition for Heroku

--- main.rb
+++ main.rb
@@ -23,6 +23,17 @@ configure :development do
   set :database, {adapter: "sqlite3", database: "database/reel_bad.db"}
 end
 
+configure :production do
+ db = URI.parse(ENV['DATABASE_URL'])
+ ActiveRecord::Base.establish_connection(
+ :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+ :host => db.host,
+ :username => db.user,
+ :password => db.password,
+ :database => db.path[1..-1],
+ :encoding => 'utf8'
+ )
+end
 
 require_relative 'database/db_setup.rb'
 
