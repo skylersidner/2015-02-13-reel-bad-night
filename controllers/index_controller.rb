20cd6e544f8e18d806b25e278a8883c2ddc4888a
Skyler Sidner <malevolentdragon@gmail.com>
2015-03-20 11:50:37 -0500

    added default error correction for homepage to function without data;

--- controllers/index_controller.rb
+++ controllers/index_controller.rb
@@ -2,13 +2,17 @@ get "/" do
   @current_event = Event.find_by current_event: 2
   # array = Event.search("current_event", 2)
   # @current_event = array[0]
-  @current_film = Film.find_by id: @current_event.film_id
+  if @current_film != nil
+    @current_film = Film.find_by id: @current_event.film_id
+  end
   # array = Film.search("id", "#{@current_event.film_id}")
   # @current_film = array[0]
   @previous_event = Event.find_by current_event: 1
   # array = Event.search("current_event", 1)
   # @previous_event = array[0]
-  @previous_film = Film.find_by id: @previous_event.film_id
+  if @previous_event != nil
+    @previous_film = Film.find_by id: @previous_event.film_id
+  end
   # array = Film.search("id", "#{@previous_event.film_id}")
   # @previous_film = array[0]
   erb :"index/homepage"
