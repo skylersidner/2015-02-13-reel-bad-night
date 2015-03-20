644e92e0c6d559b30fe9ed46f43f35cd13ccbb3d
Skyler Sidner <malevolentdragon@gmail.com>
2015-03-19 21:27:53 -0500

    moved AR DB calls to old db_setup, commented out old to see the new; still needs validations; untested;

--- database/db_setup.rb
+++ database/db_setup.rb
@@ -1,45 +1,88 @@
-require 'sqlite3'
+# require 'sqlite3'
+# DATABASE = SQLite3::Database.new("database/reel_bad.db")
 
-DATABASE = SQLite3::Database.new("database/reel_bad.db")
+unless ActiveRecord::Base.connection.table_exists?(:films)
+  ActiveRecrod::Base.connection.create_table :films do |t|
+    t.text :title
+    t.integer :year
+    t.integer :length
+    t.text :synopsis
+    t.text :trailer
+    t.integer :rt_rating
+  end
+end
 
-DATABASE.execute("CREATE TABLE IF NOT EXISTS films
-                 (id INTEGER PRIMARY KEY,
-                 title TEXT NOT NULL,
-                 year INTEGER NOT NULL CHECK (year >= 1900),
-                 length INTEGER NOT NULL CHECK(length >= 0),
-                 synopsis TEXT NOT NULL,
-                 trailer TEXT,
-                 rt_rating INTEGER)")
+# DATABASE.execute("CREATE TABLE IF NOT EXISTS films
+#                  (id INTEGER PRIMARY KEY,
+#                  title TEXT NOT NULL,
+#                  year INTEGER NOT NULL CHECK (year >= 1900),
+#                  length INTEGER NOT NULL CHECK(length >= 0),
+#                  synopsis TEXT NOT NULL,
+#                  trailer TEXT,
+#                  rt_rating INTEGER)")
 
-DATABASE.execute("CREATE TABLE IF NOT EXISTS events
-                 (id INTEGER PRIMARY KEY,
-                 start_time TEXT NOT NULL,
-                 host_msg TEXT,
-                 current_event INTEGER,
-                 film_id INTEGER,
-                 FOREIGN KEY (film_id) REFERENCES films(id))")
+unless ActiveRecord::Base.connection.table_exists?(:events)
+  ActiveRecrod::Base.connection.create_table :events do |t|
+    t.text :start_time
+    t.text :host_msg
+    t.integer :current_event
+    t.integer :film_id    
+  end
+end
 
-DATABASE.execute("CREATE TABLE IF NOT EXISTS drinks
-                 (id INTEGER PRIMARY KEY,
-                 name TEXT NOT NULL,
-                 category TEXT NOT NULL,
-                 description TEXT,
-                 event_id INTEGER,
-                 FOREIGN KEY (event_id) REFERENCES events(id))")
+# DATABASE.execute("CREATE TABLE IF NOT EXISTS events
+#                  (id INTEGER PRIMARY KEY,
+#                  start_time TEXT NOT NULL,
+#                  host_msg TEXT,
+#                  current_event INTEGER,
+#                  film_id INTEGER,
+#                  FOREIGN KEY (film_id) REFERENCES films(id))")
 
+unless ActiveRecord::Base.connection.table_exists?(:drinks)
+  ActiveRecrod::Base.connection.create_table :drinks do |t|
+    t.text :name
+    t.text :category
+    t.text :description
+    t.integer :event_id    
+  end
+end
 
-DATABASE.execute("CREATE TABLE IF NOT EXISTS patrons
-                  (id INTEGER PRIMARY KEY,
-                  first_name TEXT NOT NULL,
-                  last_name TEXT NOT NULL,
-                  username TEXT NOT NULL,
-                  password TEXT NOT NULL)")
+# DATABASE.execute("CREATE TABLE IF NOT EXISTS drinks
+#                  (id INTEGER PRIMARY KEY,
+#                  name TEXT NOT NULL,
+#                  category TEXT NOT NULL,
+#                  description TEXT,
+#                  event_id INTEGER,
+#                  FOREIGN KEY (event_id) REFERENCES events(id))")
 
-DATABASE.execute("CREATE TABLE IF NOT EXISTS events_patrons
-                 (event_id INTEGER NOT NULL,
-                 patron_id INTEGER NOT NULL,
-                 FOREIGN KEY (event_id) REFERENCES events(id),
-                 FOREIGN KEY (patron_id) REFERENCES patrons(id))")
+unless ActiveRecord::Base.connection.table_exists?(:patrons)
+  ActiveRecrod::Base.connection.create_table :patrons do |t|
+    t.text :first_name
+    t.text :last_name
+    t.text :username
+    t.text :password    
+  end
+end
 
-DATABASE.results_as_hash = true
+# DATABASE.execute("CREATE TABLE IF NOT EXISTS patrons
+#                   (id INTEGER PRIMARY KEY,
+#                   first_name TEXT NOT NULL,
+#                   last_name TEXT NOT NULL,
+#                   username TEXT NOT NULL,
+#                   password TEXT NOT NULL)")
+
+unless ActiveRecord::Base.connection.table_exists?(:events_patrons)
+  ActiveRecrod::Base.connection.create_table :events_patrons do |t|
+    t.integer :event_id
+    t.integer :patron_id 
+  end
+end
+
+# DATABASE.execute("CREATE TABLE IF NOT EXISTS events_patrons
+#                  (event_id INTEGER NOT NULL,
+#                  patron_id INTEGER NOT NULL,
+#                  FOREIGN KEY (event_id) REFERENCES events(id),
+#                  FOREIGN KEY (patron_id) REFERENCES patrons(id))")
+#
+# DATABASE.results_as_hash = true
 
